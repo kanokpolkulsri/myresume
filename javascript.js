78aba3454bc97dfeb470741a65b4cbdffc7fe1db
tonplamm <tonplam_kanokpol@hotmail.com>
2017-12-20 21:10:34 +0700

    edit style to style.css

--- javascript.js
+++ javascript.js
@@ -1,18 +1,5 @@
-var top = 0;
 function navSlide(){
-	document.getElementById('floor').style.left = -$(window).scrollTop()*0.7+"px";
+	document.getElementById('floor').style.left = -($(window).scrollTop()*0.7)+"px";
 	document.getElementById('mountain').style.left = -($(window).scrollTop()*0.4)+500+"px";
-
-
-	if(top != $(window).scrollTop()){
-		// if($(window).scrollTop() % 20 === 0){
-		// 	document.getElementById('human').src = './img/human2.png';
-		// }
-		// if($(window).scrollTop() % 40 === 0){
-		// 	document.getElementById('human').src = './img/human1.png';
-		// }
-	}
-	top = $(window).scrollTop();
 }
-
 $(window).scroll(navSlide);
